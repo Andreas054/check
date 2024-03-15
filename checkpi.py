a7961e1fd5a835ff9d16ac7d16c2c78e7778049f
Andreas054 <84636763+Andreas054@users.noreply.github.com>
2024-03-15 15:54:57 +0200

    Ver.1.1 config.py + countUsage

--- checkpi.py
+++ checkpi.py
@@ -1,45 +1,13 @@
-# Version 1.0
+# Version 1.1
 
 import fdb
 import time
 import datetime
-from guizero import *
 from threading import Thread
 
-nrCaractereProdusMAX = 20 # 4.2 + External
-# nrCaractereProdusMAX = 9 # 3.5
+from config import *
 
-# dbdir = "172.16.0.154:D:/IBData/SMARTCASH.FDB"
-dbdir = "192.168.10.100:D:/FBData/SMARTCASH.FDB"
-# dbdir = "192.168.40.100:D:/IBData/SMARTCASH.FDB"
-# dbdir = "192.168.100.100:D:/IBData/SMARTCASH.FDB"
-# dbdir = "192.168.0.100:D:/IBData/SMARTCASH.FDB"
-
-folderProgram = "/home/pi/checkpi/"
-
-# Exteral Monitor
-# app = App(width = 1000, height = 500, title = "checkPrice", bg = "white")
-# text0 = Text(app, text = " ", size = 80, font = "Times New Roman", color = "blue")
-# Picture = Picture(app, image = f"{folderProgram}logoExternal.png")
-# text1 = Text(app, text = "Scanati produsul", size = 70, font = "Times New Roman", color = "blue")
-# text2 = Text(app, text = "aici!", size = 70, font = "Times New Roman", color = "blue")
-# text3 = Text(app, text = "...", size = 80, font = "Times New Roman", color = "red")
-
-# 3.5 LCD
-# app = App(width = 1000, height = 500, title = "checkPrice", bg = "white")
-# text0 = Text(app, text = " ", size = 10, font = "Times New Roman", color = "blue")
-# Picture = Picture(app, image = f"{folderProgram}logo35.png")
-# text1 = Text(app, text = "Scanati produsul", size = 35, font = "Times New Roman", color = "blue")
-# text2 = Text(app, text = "aici!", size = 35, font = "Times New Roman", color = "blue")
-# text3 = Text(app, text = "...", size = 55, font = "Times New Roman", color = "red")
-
-# 4.2 LCD
-app = App(width = 1000, height = 500, title = "checkPrice", bg = "white")
-text0 = Text(app, text = " ", size = 20, font = "Times New Roman", color = "blue")
-Picture = Picture(app, image = f"{folderProgram}logo42.png")
-text1 = Text(app, text = "Scanati produsul", size = 40, font = "Times New Roman", color = "blue")
-text2 = Text(app, text = "aici!", size = 40, font = "Times New Roman", color = "blue")
-text3 = Text(app, text = "...", size = 70, font = "Times New Roman", color = "red")
+countScannerUsage = 0
 
 def barcode_reader():
     """Barcode code obtained from 'brechmos' 
@@ -88,6 +56,19 @@ def barcode_reader():
                         ss += hid[int(c)]
     return ss
 
+def checkTimeSaveLog():
+    global countScannerUsage
+
+    while True:
+        now = datetime.datetime.now()
+        currentTime = int(now.strftime("%H%M"))
+        if 2150 - currentTime <= 30:
+            with open(folderProgram + "checkLog.txt", "a") as logFile:
+                logFile.write(f"[{datetime.datetime.now()}] : Nr citiri = {countScannerUsage}\n")
+            import sys
+            sys.exit() # exit Thread
+        time.sleep(30 * 60)
+
 def separaParagrafPeLinii(paragraf, maxWordLength):
     tmpRezultat = ""
     tmpRezultatLista = []
@@ -109,7 +90,7 @@ def separaParagrafPeLinii(paragraf, maxWordLength):
     return paragraf
 
 def usbScan():
-    global messageReceivedBool
+    global countScannerUsage
     while True:
         output = barcode_reader()
         #output = "5942325000233"
@@ -122,7 +103,9 @@ def usbScan():
 
         fdbCursor1.execute(fdbSQLCommand)
         listaCursor1 = fdbCursor1.fetchall()
-        
+
+        countScannerUsage += 1
+
         if len(listaCursor1) == 0:
             print("cod inexistent")
             text1.value = "Articol inexistent"
@@ -156,10 +139,13 @@ def usbScan():
 
 # Write to log current date
 with open(folderProgram + "checkLog.txt", "a") as logFile:
-    logFile.write(f"\n[{datetime.datetime.now()}] : Started program\n")
+    logFile.write(f"[{datetime.datetime.now()}] : Started program\n")
 
 threadusbScan = Thread(target = usbScan)
 threadusbScan.start()
+
+threadcheckTimeSaveLog = Thread(target = checkTimeSaveLog)
+threadcheckTimeSaveLog.start()
 
 app.set_full_screen()
 app.display()
---
+++ config.py
@@ -0,0 +1,40 @@
+from guizero import *
+
+folderProgram = "/home/pi/checkpi/"
+
+
+nrCaractereProdusMAX = 20 # 4.2 + External
+# nrCaractereProdusMAX = 9 # 3.5
+
+
+dbdir = "192.168.10.100:D:/FBData/04/SMARTCASH.FDB"
+# dbdir = "192.168.40.100:D:/IBData/SMARTCASH.FDB"
+# dbdir = "192.168.100.100:D:/IBData/SMARTCASH.FDB"
+# dbdir = "192.168.150.100:D:/FBData/04/SMARTCASH.FDB"
+# dbdir = "192.168.0.100:D:/IBData/SMARTCASH.FDB"
+
+#######################################################################
+# Exteral Monitor
+# app = App(width = 1000, height = 500, title = "checkPrice", bg = "white")
+# text0 = Text(app, text = " ", size = 80, font = "Times New Roman", color = "blue")
+# Picture = Picture(app, image = f"{folderProgram}logoExternal.png")
+# text1 = Text(app, text = "Scanati produsul", size = 70, font = "Times New Roman", color = "blue")
+# text2 = Text(app, text = "aici!", size = 70, font = "Times New Roman", color = "blue")
+# text3 = Text(app, text = "...", size = 80, font = "Times New Roman", color = "red")
+
+# 3.5 LCD
+# app = App(width = 1000, height = 500, title = "checkPrice", bg = "white")
+# text0 = Text(app, text = " ", size = 10, font = "Times New Roman", color = "blue")
+# Picture = Picture(app, image = f"{folderProgram}logo35.png")
+# text1 = Text(app, text = "Scanati produsul", size = 35, font = "Times New Roman", color = "blue")
+# text2 = Text(app, text = "aici!", size = 35, font = "Times New Roman", color = "blue")
+# text3 = Text(app, text = "...", size = 55, font = "Times New Roman", color = "red")
+
+# 4.2 LCD
+app = App(width = 1000, height = 500, title = "checkPrice", bg = "white")
+text0 = Text(app, text = " ", size = 20, font = "Times New Roman", color = "blue")
+Picture = Picture(app, image = f"{folderProgram}logo42.png")
+text1 = Text(app, text = "Scanati produsul", size = 40, font = "Times New Roman", color = "blue")
+text2 = Text(app, text = "aici!", size = 40, font = "Times New Roman", color = "blue")
+text3 = Text(app, text = "...", size = 70, font = "Times New Roman", color = "red")
+#######################################################################
